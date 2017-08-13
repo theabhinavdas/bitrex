dbb95e2eecd92226e048b68e7be73ad2f81534cf
Vinay Agarwal <vinay035@gmail.com>
2017-08-13 13:46:42 +0530

    added 3 more crons

---
+++ public/cron_30_min.php
@@ -0,0 +1,30 @@
+<?php
+
+define('CORNJOB', TRUE);
+include("index.php");
+
+
+$link="https://bittrex.com/api/v1.1/public/getmarketsummaries";
+$buy_result = @file_get_contents($link);
+$buy_decoded = json_decode($buy_result, true);
+
+if(count($buy_decoded['result'])>0)
+{
+    foreach($buy_decoded['result'] as $result)
+    {       
+        $summary = new Summary30min();
+        $summary->market_name=$result['MarketName'];
+        $summary->volume=$result['Volume'];
+        $summary->high=$result['High'];
+        $summary->low=$result['Low'];
+        $summary->bid=$result['Bid'];
+        $summary->ask=$result['Ask'];
+        $summary->timestamp=$result['TimeStamp'];
+        $summary->last=$result['Last'];
+        $summary->open_buy_orders=$result['OpenBuyOrders'];
+        $summary->open_sell_orders=$result['OpenSellOrders'];
+        $summary->save();        
+    }   
+}
+
+?>
---
+++ public/protected/models/Summary30min.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This is the model class for table "BitcoinPrice".
+ *
+ * The followings are the available columns in table 'BitcoinPrice':
+ * @property integer $id
+ * @property string $currency
+ * @property double $price
+ * @property string $last_updated
+ */
+class Summary30min extends CActiveRecord {
+
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return BitcoinPrice the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'Summary30min';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(            
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id,open,low,high,timestamp,market_name,volume,timestamp,bid,ask,open_buy_orders,open_sell_orders,last', 'safe'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        
+    }
+    
+    public function get_summary($market)
+    {
+        $criteria=new CDbCriteria;
+        $criteria->compare('market_name',$market);
+        $criteria->order='timestamp DESC';
+        $data= Summary30min::model()->findAll($criteria);
+        return $data;
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search() {
+       
+    }
+    
+
+}
